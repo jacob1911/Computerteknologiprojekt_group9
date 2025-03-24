55ab99cf10bfea8cc1d23f1d1c1b06480d94d999
Sebastian Kittelmann <sebkittel123@gmail.com>
2025-03-24 12:14:01 +0100

    Co-authored-by: GamerJacob <jacob1911@users.noreply.github.com>

--- turtlebot3_obstacle_detection.py
+++ turtlebot3_obstacle_detection.py
@@ -22,8 +22,8 @@ from rclpy.node import Node
 from rclpy.qos import qos_profile_sensor_data
 from rclpy.qos import QoSProfile
 from sensor_msgs.msg import LaserScan
-import time
-import math
+import time as t
+import numpy as np
 
 class Turtlebot3ObstacleDetection(Node):
 
@@ -35,14 +35,23 @@ class Turtlebot3ObstacleDetection(Node):
         print('stop distance: 0.5 m')
         print('----------------------------------------------')
 
+
         self.scan_ranges = []
+        self.divisions = 20
         self.has_scan_received = False
+        
+        self.stop_distance = 0.2
+        self.start_turning = 0.7
+        self.max_speed = 0.2
+        
+        self.start_time = self.get_clock().now().nanoseconds / 1e9
+        self.run_time = 10
 
-        self.stop_distance = 0.5
         self.tele_twist = Twist()
-        self.tele_twist.linear.x = 0.2
+        self.tele_twist.linear.x = self.max_speed
+        self.max_angle = 1.28
         self.tele_twist.angular.z = 0.0
-
+        self.turn_factor = 1.
         qos = QoSProfile(depth=10)
 
         self.cmd_vel_pub = self.create_publisher(Twist, 'cmd_vel', qos)
@@ -64,6 +73,7 @@ class Turtlebot3ObstacleDetection(Node):
     def scan_callback(self, msg):
         self.scan_ranges = msg.ranges
         
+        # Setting all bad values, inf and nan, to 3.5
         for i in range(len(self.scan_ranges)):
             cur_val = self.scan_ranges[i]
             if (not (0 < cur_val and cur_val <= 3.5)):
@@ -75,34 +85,92 @@ class Turtlebot3ObstacleDetection(Node):
         self.tele_twist = msg
 
     def timer_callback(self):
+        current_time = self.get_clock().now().nanoseconds / 1e9
+        
+        if int(current_time - self.start_time) > self.run_time:
+            self.get_logger().info("Time limit reached. Shutting down node.")
+            rclpy.shutdown()
+            return
+        
         if self.has_scan_received:
             self.detect_obstacle()
 
+    # Lineær udvikling af konstanten
+    def angle_factor(self,front_d):
+        x = abs(self.start_turning - front_d)
+        return x / (self.start_turning-self.stop_distance)
+    
+    def speed_factor(self, front_d):
+        if(front_d > self.start_turning):
+            return 1.
+        return front_d / (self.start_turning - self.stop_distance)
+
+
+    # Afgører om robotten skal dreje højre eller venstre om obstacle
+    def set_rot_direction(self, r_dist, l_dist):
+        for i in range(self.divisions // 4 - 1):
+            if(self.start_turning / 1.5 < min(r_dist[i], r_dist[i+1])):
+                return -1.
+            elif(self.start_turning / 1.5 < min(l_dist[i], l_dist[i+1])):
+                return 1.
+        
+        min_l = min(l_dist[1], l_dist[2])
+        min_r = min(r_dist[1], r_dist[2])
+        if(min_l < min_r):
+            self.turn_factor = -1.
+
+        else:
+            self.turn_factor = 1.
+       
+        
+
+    
+
     def detect_obstacle(self):
-        left_range = int(len(self.scan_ranges) / 4)
-        right_range = int(len(self.scan_ranges) * 3 / 4)
-        front_range = 25
 
-        mid_index = int(len(self.scan_ranges) / 2)
-        front_distance = min(self.scan_ranges[mid_index - front_range: mid_index + front_range]) 
+        len_scan_range = len(self.scan_ranges)
+        range_size = int(len_scan_range / self.divisions)
 
-        # if(math.isnan(front_distance)):
-        #     front_distance = 3.5
+        left_distances = []
+        for i in range(self.divisions // 4):
+            min_val = min(self.scan_ranges[range_size * i: range_size * (i + 1)])
+            left_distances.append(min_val)
 
-        obstacle_distance = min(
-            min(self.scan_ranges[0:left_range]),
-            min(self.scan_ranges[right_range:360])
-        )
-        print(f"Front distance: {front_distance}")
+        right_distances = []
+        for i in range(self.divisions // 4):
+            min_val = min(self.scan_ranges[(len_scan_range-1) - range_size * (i + 1) : (len_scan_range-1) - range_size * i])
+            right_distances.append(min_val)
+        
+        front_distance = min([left_distances[0], right_distances[0]])
+
+        # ## TIDLIGERE VERSION ##front_distance = min(self.scan_ranges[mid_index - front_range: mid_index + front_range]) 
+
+
         twist = Twist()
+        # 1) Checks if it should stop
         if front_distance < self.stop_distance:
             twist.linear.x = 0.0
-            twist.angular.z = 0.2
+            self.set_rot_direction(right_distances, left_distances)
+            twist.angular.z = self.max_angle * self.turn_factor
             self.get_logger().info('Obstacle detected! Stopping.', throttle_duration_sec=2)
+
+        # 2) Checks how hard it should turn
+        elif front_distance < self.start_turning:
+            twist.linear.x = self.max_speed * self.speed_factor(front_distance)
+            # print(f"Linear speed: {twist.linear.x}, front distance: {front_distance}")
+            self.set_rot_direction(right_distances, left_distances)
+            twist.angular.z = self.max_angle * self.angle_factor(front_distance) * self.turn_factor
+                  
         else:
             twist = self.tele_twist
 
         self.cmd_vel_pub.publish(twist)
+
+
+        
+        
+
+        
 
 
 def main(args=None):
